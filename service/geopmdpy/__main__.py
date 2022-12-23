69c2942530fb32082bc95062a8967128b70e732d
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2022-12-22 20:00:07 -0800

    Enable grpc as an alternative to DBus for communications
    - First pass at creating a prototype buffer definition
      for DBus methods used by ServiceIOGroup
    - First draft of GEOPM grpc service
    - Generate protobuffer files with autogen.sh
    - Add some configure scripts for grpc.
    - Add grpc requirements to spec file
    - Add protoc-gen.sh to tarball
    - Fixes #2775
    - Use LOCAL_TCP not UDS because python support server context in UDS is
      limited
    - Abstract use of GLib and posix pid interfaces. This will enable network
      peer ID to be used in place of PID for tracking clients
    - Derive the client_id from the gRPC server context: use number
      in peer name following last colon
    - TODO: Write unit tests
    - TODO: Run integration tests for controls
    - TODO: Add documentation
    - TODO: Implement session closure when client connection ends
    
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/geopmdpy/__main__.py
+++ service/geopmdpy/__main__.py
@@ -2,11 +2,16 @@
 #  SPDX-License-Identifier: BSD-3-Clause
 #
 
+import sys
+
 from dasbus.loop import EventLoop
 from dasbus.connection import SystemMessageBus
 from signal import signal
 from signal import SIGTERM
+
 from . import service
+from . import system_files
+from . import grpc_service
 
 _bus = None
 
@@ -17,9 +22,8 @@ def term_handler(signum, frame):
 def stop():
     if _bus is not None:
         _bus.disconnect()
-    exit(0)
 
-def main():
+def main_dbus():
     signal(SIGTERM, term_handler)
     loop = EventLoop()
     global _bus
@@ -30,6 +34,15 @@ def main():
         loop.run()
     finally:
         stop()
+
+def main_grpc():
+    grpc_service.run()
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == '--grpc':
+        main_grpc()
+    else:
+        main_dbus()
 
 if __name__ == '__main__':
     main()
