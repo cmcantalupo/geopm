f9d5811d432de783ec4e4974f73c5853a4ae0474
Alejandro Vilches <alejandro.vilches@intel.com>
2023-03-30 16:27:13 -0700

    Add first draft of integration test for multiple apps
    Signed-off-by: Alejandro Vilches <alejandro.vilches@intel.com>

--- integration/test/Makefile.mk
+++ integration/test/Makefile.mk
@@ -15,7 +15,9 @@ EXTRA_DIST += integration/test/check_trace.py \
               integration/test/test_template.py.in \
               integration/test/util.py \
               integration/test/test_tutorial_python_agents.py \
-              integration/test/test_application_totals_pinning.py
+              integration/test/test_application_totals_pinning.py \
+              integration/test/test_multi_app.py \
+              integration/test/test_multi_app.sh \
               # end
 
 include integration/test/test_enforce_policy.mk
