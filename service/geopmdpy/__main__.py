cf6864d06ce8656e0436b6ff52b608125b0e264f
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2023-05-10 14:52:24 -0700

    REVERT: Add profiling to geopmd
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- service/geopmdpy/__main__.py
+++ service/geopmdpy/__main__.py
@@ -6,6 +6,7 @@ from dasbus.loop import EventLoop
 from dasbus.connection import SystemMessageBus
 from signal import signal
 from signal import SIGTERM
+from line_profiler import LineProfiler
 from . import service
 
 _bus = None
@@ -29,9 +30,36 @@ def main():
     _loop = EventLoop()
     _bus = SystemMessageBus()
     try:
+        bus_object = service.GEOPMService()
+        method_names = [nn for nn in dir(bus_object) if not nn.startswith('_')]
+        prof = LineProfiler()
+        prof.add_function(bus_object._platform.get_group_access)
+        prof.add_function(bus_object._platform.set_group_access)
+        prof.add_function(bus_object._platform.set_group_access_signals)
+        prof.add_function(bus_object._platform.set_group_access_controls)
+        prof.add_function(bus_object._platform.get_user_access)
+        prof.add_function(bus_object._platform.get_all_access)
+        prof.add_function(bus_object._platform.get_signal_info)
+        prof.add_function(bus_object._platform.get_control_info)
+        prof.add_function(bus_object._platform.lock_control)
+        prof.add_function(bus_object._platform.unlock_control)
+        prof.add_function(bus_object._platform.open_session)
+        prof.add_function(bus_object._platform.close_session)
+        prof.add_function(bus_object._platform.close_session_admin)
+        prof.add_function(bus_object._platform._close_session_completely)
+        prof.add_function(bus_object._platform._close_session_write)
+        prof.add_function(bus_object._platform.start_batch)
+        prof.add_function(bus_object._platform.stop_batch)
+        prof.add_function(bus_object._platform.read_signal)
+        prof.add_function(bus_object._platform.write_control)
+        prof.add_function(bus_object._platform.start_profile)
+        prof.add_function(bus_object._platform.stop_profile)
+        prof.add_function(bus_object._platform.get_profile_pids)
+        prof.add_function(bus_object._platform.get_profile_region_names)
+        prof.add_function(bus_object._topo.get_cache)
         _bus.publish_object("/io/github/geopm", service.GEOPMService())
         _bus.register_service("io.github.geopm")
-        _loop.run()
+        prof.run("_loop.run()")
     finally:
         stop()
 
