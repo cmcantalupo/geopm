d556978877db394d0751e840fcb3738c857d5219
Daniel Wilson <daniel1.wilson@intel.com>
2023-12-01 13:58:53 -0800

    Implement cpufreq sysfs driver

--- service/src/IOGroup.cpp
+++ service/src/IOGroup.cpp
@@ -139,8 +139,8 @@ namespace geopm
 #endif
         register_plugin(ConstConfigIOGroup::plugin_name(),
                         ConstConfigIOGroup::make_plugin);
-	register_plugin(CpufreqSysfsDriver::plugin_name(),
-			CpufreqSysfsDriver::make_plugin);
+        register_plugin(CpufreqSysfsDriver::plugin_name(),
+                        CpufreqSysfsDriver::make_plugin);
     }
 
     IOGroupFactory &iogroup_factory(void)
